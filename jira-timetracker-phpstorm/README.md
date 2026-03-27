326f57832bc7350533c9bb73995e8544a3f686ba
mayzervan9 <mayzervan9@gmail.com>
2026-03-27 13:47:46 +0200

    Initial commit: Jira Time Tracker plugin

---
+++ jira-timetracker-phpstorm/README.md
@@ -0,0 +1,10 @@
+# Jira Time Tracker (PhpStorm plugin)
+
+MVP IntelliJ Platform plugin for integrating with Jira and tracking worklogs.
+
+## Dev
+
+- Open this folder in IntelliJ IDEA / PhpStorm.
+- Use Gradle tasks:
+  - `runIde` to start a sandbox IDE with the plugin.
+  - `buildPlugin` to build a distributable ZIP.
---
+++ jira-timetracker-phpstorm/build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    kotlin("jvm") version "2.0.21"
+    id("org.jetbrains.intellij.platform") version "2.2.1"
+}
+
+group = providers.gradleProperty("pluginGroup").get()
+version = providers.gradleProperty("pluginVersion").get()
+
+repositories {
+    mavenCentral()
+    intellijPlatform {
+        defaultRepositories()
+    }
+}
+
+dependencies {
+    intellijPlatform {
+        phpstorm(providers.gradleProperty("platformVersion"))
+        bundledPlugin("com.intellij.platform.images")
+    }
+
+    implementation(kotlin("stdlib"))
+    implementation("org.json:json:20240303")
+}
+
+intellijPlatform {
+    pluginConfiguration {
+        name = providers.gradleProperty("pluginName")
+        version = providers.gradleProperty("pluginVersion")
+    }
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
